6915bcc890012e3ea3e776ea0212793dd162d2b7
Bahram Shamshiri <jowharshamshiri@gmail.com>
2025-03-16 10:51:12 +0100

    Enhance Jest configuration; add jest-summary-reporter and enable silent mode

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 export default {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-	extensionsToTreatAsEsm: ['.ts'],
-	moduleNameMapper: {
-	  '^(\\.{1,2}/.*)\\.js$': '$1',
-	},
-	transform: {
-	  '^.+\\.(ts|tsx)$': ['ts-jest', {
-		useESM: true,
-	  }],
-	},
-	setupFilesAfterEnv: ['./jest.setup.ts'],
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { useESM: true }],
+  },
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  reporters: ["default", "jest-summary-reporter"],
+  silent: true,
+};
